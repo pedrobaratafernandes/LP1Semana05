c9f17bf3bd0971d02de954229ad7e58a923a2627
Pedro Fernandes <pedro.barata.fernandes@gmail.com>
2026-03-18 15:21:59 +0000

    Add LevelTwo

---
+++ LevelTwo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Spectre.Console;
+
+namespace LevelTwo
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string image_file;
+            int size_width;
+            
+            if (args.Length < 2)
+            {
+                image_file = "tux.jpg";
+                size_width = 24;
+            }
+            else
+            {
+                image_file = args[0];
+                size_width = int.Parse(args[1]);
+            }
+            CanvasImage image = new CanvasImage(image_file);
+            image.MaxWidth(size_width);
+            AnsiConsole.Write(image);
+        }
+    }
+}
