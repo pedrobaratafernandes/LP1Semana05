e29534d6bfb5e21f9f8964e5c65be65c8c697821
Pedro Fernandes <pedro.barata.fernandes@gmail.com>
2026-03-19 16:09:47 +0000

    2

--- DungeonStats/Program.cs
+++ DungeonStats/Program.cs
@@ -6,7 +6,29 @@ namespace DungeonStats
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+
+        }
+
+        private static int Damage(int attack, int defense)
+        {
+            return attack - defense;
+        }
+        private static int Damage(int attack)
+        {
+            return damage;
+        }
+        private static int CriticalHit(int damage)
+        {
+
+            if (damage <= 0)
+            {
+                damage = 0;
+            }
+            else
+            {
+                damage = 1 + Damage(damage);
+            }
+            return damage;
         }
     }
 }
