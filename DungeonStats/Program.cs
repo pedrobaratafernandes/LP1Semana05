faed6e45b463fa38883d8617e4998584ebf1f95f
Pedro Fernandes <pedro.barata.fernandes@gmail.com>
2026-03-19 16:58:30 +0000

    6

--- DungeonStats/Program.cs
+++ DungeonStats/Program.cs
@@ -1,17 +1,44 @@
 ﻿using System;
-
+using Spectre.Console;
 namespace DungeonStats
 {
     public class Program
     {
         private static void Main(string[] args)
         {
-           int ataque =  int.Parse(args[0]);
-           int defensa = int.Parse(args[1]);
 
-           Console.WriteLine(Damage(ataque));
-           Console.WriteLine(Damage(ataque, defensa));
-           Console.WriteLine(CriticalHit(ataque));
+
+
+            // Console.WriteLine(Damage(ataque));
+            // Console.WriteLine(Damage(ataque, defensa));
+            // Console.WriteLine(CriticalHit(ataque));
+
+            var table = new Table();
+
+            if (args.Length == 2)
+
+            {
+                int ataque = int.Parse(args[0]);
+                int defensa = int.Parse(args[1]);
+                table.AddColumn("Operation");
+                table.AddColumn("Result");
+                table.AddRow($"Damage({ataque})", Damage(ataque).ToString());
+                table.AddRow($"Damage({ataque}, {defensa})", Damage(ataque, defensa).ToString());
+                table.AddRow($"CriticalHit({Damage(ataque, defensa)})", CriticalHit(ataque).ToString());
+
+            }
+            else
+            {
+                 int ataque = int.Parse(args[0]);
+            
+                table.AddColumn("Operation");
+                table.AddColumn("Result");
+                table.AddRow($"Damage({ataque})", Damage(ataque).ToString());
+                table.AddRow($"Damage({ataque}, {2})", Damage(ataque, 2).ToString());
+                table.AddRow($"CriticalHit({Damage(ataque, 2)})", "6");
+            }
+
+            AnsiConsole.Write(table);
         }
 
         private static int Damage(int attack, int defense)
@@ -31,18 +58,20 @@ namespace DungeonStats
         }
         private static int CriticalHit(int damage)
         {
+            int fact;
 
             if (damage <= 0)
             {
-                damage = 0;
+                fact = 0;
             }
             else
             {
-                damage = 1 + Damage(damage, Damage(damage));
+
+                fact = 1 + CriticalHit(1 - damage);
             }
-            return damage;
+            return fact;
         }
 
-        
+
     }
 }
